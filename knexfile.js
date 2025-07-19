069f7d0f668bf33d0164ef3f8a9224e3919eb76d
Ola Abdulrazaq <abdulrazaq2a@gmail.com>
2025-07-19 18:38:18 +0100

    Update production env data

--- knexfile.js
+++ knexfile.js
@@ -17,6 +17,19 @@ const config = {
       directory: "./migrations",
     },
   },
+  production: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+  },
 };
+
 
 module.exports = config;
